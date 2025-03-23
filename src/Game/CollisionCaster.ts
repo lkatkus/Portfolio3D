ff35e77e576f370eac648b434ddd93a199203e87
lkatkus <laimonas@katkus.eu>
2025-03-23 11:29:45 +0200

    added CollisionCaster for basic collisions

---
+++ src/Game/CollisionCaster.ts
@@ -0,0 +1,119 @@
+import * as THREE from "three";
+import type { Player } from "./Player";
+
+const frontTo = {
+  back: (f: THREE.Vector3) => f.clone().negate(),
+  left: (f: THREE.Vector3) => new THREE.Vector3(f.z, 0, -f.x),
+  right: (f: THREE.Vector3) => new THREE.Vector3(-f.z, 0, f.x),
+  up: (_f: THREE.Vector3) => new THREE.Vector3(0, 1, 0),
+  down: (_f: THREE.Vector3) => new THREE.Vector3(0, -1, 0),
+};
+
+const colors = {
+  front: "red",
+  back: "green",
+  left: "blue",
+  right: "yellow",
+  up: "magenta",
+  down: "cyan",
+};
+
+export class CollisionCaster {
+  player: Player;
+  origin: THREE.Vector3;
+
+  private front: THREE.Raycaster;
+  private back: THREE.Raycaster;
+  private left: THREE.Raycaster;
+  private right: THREE.Raycaster;
+  private up: THREE.Raycaster;
+  private down: THREE.Raycaster;
+
+  private debugArrows: Record<string, THREE.ArrowHelper> = {};
+
+  constructor(
+    player: Player,
+    origin: THREE.Vector3,
+    forwardDirection: THREE.Vector3
+  ) {
+    this.player = player;
+    this.origin = origin;
+
+    this.front = this.initRayCaster(forwardDirection);
+    this.back = this.initRayCaster(frontTo.back(forwardDirection));
+    this.left = this.initRayCaster(frontTo.left(forwardDirection));
+    this.right = this.initRayCaster(frontTo.right(forwardDirection));
+    this.up = this.initRayCaster(frontTo.up(forwardDirection));
+    this.down = this.initRayCaster(frontTo.down(forwardDirection));
+
+    this.initDebug();
+  }
+
+  initRayCaster(direction: THREE.Vector3) {
+    const rayCaster = new THREE.Raycaster(
+      this.origin.clone(),
+      direction.clone().normalize(),
+      0,
+      1
+    );
+
+    return rayCaster;
+  }
+
+  initDebug() {
+    const { group } = this.player;
+
+    const directions = {
+      front: this.front.ray.direction,
+      back: this.back.ray.direction,
+      left: this.left.ray.direction,
+      right: this.right.ray.direction,
+      up: this.up.ray.direction,
+      down: this.down.ray.direction,
+    };
+
+    for (const key in directions) {
+      const arrow = new THREE.ArrowHelper(
+        directions[key as keyof typeof directions],
+        this.origin,
+        1,
+        colors[key as keyof typeof colors]
+      );
+
+      group.add(arrow);
+
+      this.debugArrows[key as keyof typeof this.debugArrows] = arrow;
+    }
+  }
+
+  update() {
+    const { player } = this;
+
+    const playerPosition = player.group.position.clone();
+    playerPosition.y += 1;
+
+    this.origin.copy(playerPosition);
+
+    const forwardDirection = this.player.orientation.clone().normalize();
+
+    this.front.set(this.origin, forwardDirection);
+    this.back.set(this.origin, frontTo.back(forwardDirection));
+    this.left.set(this.origin, frontTo.left(forwardDirection));
+    this.right.set(this.origin, frontTo.right(forwardDirection));
+    this.up.set(this.origin, frontTo.up(forwardDirection));
+    this.down.set(this.origin, frontTo.down(forwardDirection));
+  }
+
+  checkCollisions() {
+    const sceneObjects = this.player.game.scenographer.group.children;
+
+    return {
+      front: this.front.intersectObjects(sceneObjects, true).length > 0,
+      back: this.back.intersectObjects(sceneObjects, true).length > 0,
+      left: this.left.intersectObjects(sceneObjects, true).length > 0,
+      right: this.right.intersectObjects(sceneObjects, true).length > 0,
+      up: this.up.intersectObjects(sceneObjects, true).length > 0,
+      down: this.down.intersectObjects(sceneObjects, true).length > 0,
+    };
+  }
+}
--- src/Game/Player.ts
+++ src/Game/Player.ts
@@ -1,23 +1,26 @@
 import * as THREE from "three";
 import { Entity } from "./Entity";
 import type { Game } from "./Game";
+import { CollisionCaster } from "./CollisionCaster";
+
+const INITIAL_FORWARD = new THREE.Vector3(-1, 0, 0);
 
 export class Player extends Entity {
   game: Game;
   isControlled: boolean;
   orientation: THREE.Vector3;
-  movementDirection: THREE.Vector3;
   rotationSpeed: number;
   moveSpeed: number;
   keysPressed: Set<string>;
+
+  collisionCaster: CollisionCaster;
 
   constructor(game: Game) {
     super("player", "/models/act-1/act-1-button-start.glb");
 
     this.game = game;
     this.isControlled = false;
-    this.orientation = new THREE.Vector3(0, 0, -1); // Correct forward direction
-    this.movementDirection = new THREE.Vector3();
+    this.orientation = INITIAL_FORWARD.clone();
 
     this.rotationSpeed = 0.05;
     this.moveSpeed = 10;
@@ -28,7 +31,8 @@ export class Player extends Entity {
 
     this.init();
     this.initControls();
-    this.initDebug();
+
+    this.collisionCaster = this.initCollisionCaster();
   }
 
   async init() {
@@ -59,6 +63,17 @@ export class Player extends Entity {
     window.addEventListener("keyup", this.handleKeyUp.bind(this));
   }
 
+  initCollisionCaster() {
+    const { group, orientation } = this;
+
+    const playerPosition = group.position.clone();
+    const playerDirection = orientation.clone();
+
+    playerPosition.y += 1;
+
+    return new CollisionCaster(this, playerPosition, playerDirection);
+  }
+
   enableControls() {
     this.isControlled = true;
   }
@@ -69,15 +84,20 @@ export class Player extends Entity {
   }
 
   updateOrientation() {
-    const forward = new THREE.Vector3(0, 0, -1);
+    const forward = INITIAL_FORWARD.clone();
 
     forward.applyQuaternion(this.group.quaternion);
     this.orientation.copy(forward);
   }
 
   update() {
-    const { clock } = this.game;
+    const { game, collisionCaster } = this;
+    const { clock } = game;
     const deltaTime = clock.deltaTime;
+
+    collisionCaster.update();
+
+    const collisions = collisionCaster.checkCollisions();
 
     // Handle rotation
     if (this.keysPressed.has("a") || this.keysPressed.has("d")) {
@@ -96,30 +116,38 @@ export class Player extends Entity {
     if (this.keysPressed.has("ArrowUp") || this.keysPressed.has("ArrowDown")) {
       const moveVector = new THREE.Vector3(0, 1, 0);
 
-      if (this.keysPressed.has("ArrowDown")) {
-        moveVector.negate();
+      if (this.keysPressed.has("ArrowUp") && !collisions.up) {
+        moveVector.normalize().multiplyScalar(this.moveSpeed * deltaTime);
+
+        this.group.position.add(moveVector);
       }
 
-      moveVector.normalize().multiplyScalar(this.moveSpeed * deltaTime);
+      if (this.keysPressed.has("ArrowDown") && !collisions.down) {
+        moveVector.negate();
+        moveVector.normalize().multiplyScalar(this.moveSpeed * deltaTime);
 
-      this.group.position.add(moveVector);
+        this.group.position.add(moveVector);
+      }
     }
 
-    // Handle movement
+    // Handle horizontal movement
+
     if (this.keysPressed.has("w") || this.keysPressed.has("s")) {
       const moveVector = new THREE.Vector3();
 
-      if (this.keysPressed.has("w")) {
-        moveVector.add(this.orientation.clone().negate());
-      }
-
-      if (this.keysPressed.has("s")) {
+      if (this.keysPressed.has("w") && !collisions.front) {
         moveVector.add(this.orientation);
+        moveVector.normalize().multiplyScalar(this.moveSpeed * deltaTime);
+
+        this.group.position.add(moveVector);
       }
 
-      moveVector.normalize().multiplyScalar(this.moveSpeed * deltaTime);
+      if (this.keysPressed.has("s") && !collisions.back) {
+        moveVector.add(this.orientation.clone().negate());
+        moveVector.normalize().multiplyScalar(this.moveSpeed * deltaTime);
 
-      this.group.position.add(moveVector);
+        this.group.position.add(moveVector);
+      }
     }
   }
 }
